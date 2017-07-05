830600cb477f182083cb8ef0389a7d0266f4877f
villellasullivan <villellasullivan@gmail.com>
2017-07-05 10:46:55 +0200

    Get 3 circuit for home

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -6,11 +6,30 @@
     .module('app')
     .controller('HomeController', HomeController);
 
-  HomeController.$inject = ['$scope'];
+  HomeController.$inject = ['$scope','$http'];
 
-  function HomeController($scope) {
+  function HomeController($scope,$http) {
 
+    $scope.getDestinations = {};
+    var config = {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+    }
 
-  }
-
+    $http({
+        method: 'GET',
+        url: 'http://localhost:5000/threecircuit'
+      }, config)
+      .then(function successCallback(response) {
+          if(response.status == 200) {
+            $scope.getDestinations = response.data;
+            console.log(response.data);
+          }
+      },
+      function errorCallback(response) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+      });
+    };
 }());
