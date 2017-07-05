51cd25f508076db392257a6fdb35cb1dd73d5f33
villellasullivan <villellasullivan@gmail.com>
2017-07-05 09:48:25 +0200

    Add api

---
+++ api/gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/style.css' : 'sass/style.scss'
+        }
+      }
+    }
+}
---
+++ api/public/js/readme.txt
@@ -0,0 +1,4 @@
+aller sur page profile
+recuperer code source
+chercher chaque statut
+parse + afficher
